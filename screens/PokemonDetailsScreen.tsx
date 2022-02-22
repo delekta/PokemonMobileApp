8a6a830e067293ac082cc58c6242cb7cfaaf114c
Kamil Delekta <kamil.delekta@swmansion.com>
2022-02-22 16:34:21 +0100

    Receiving Correct Props In Pokemon Details

--- screens/PokemonDetailsScreen.tsx
+++ screens/PokemonDetailsScreen.tsx
@@ -1,20 +1,13 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
-import { StackNavigationProp } from '@react-navigation/stack';
-import { StackNavigationState } from '@react-navigation/native';
+import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
+import { RouteProp, StackNavigationState } from '@react-navigation/native';
+import { RootStackParamList } from '../components/TabNavigator';
 
-interface Pokemon {
-    name: string;
-    url: string;
-}
+type Props = StackScreenProps<RootStackParamList, 'PokemonDetails'>
 
-type PokemonProp = {
-    pokemon: Pokemon;
-};
-
-export class PokemonDetailsScreen extends Component<PokemonProp> {
+export class PokemonDetailsScreen extends Component<Props> {
     render() {
-        console.log(this.props)
         return (
             <View>
                 <Text>{this.props.route.params.pokemon.name} Details!</Text>
