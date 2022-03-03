ffd01e4aed6da35fc9964cbc748afced9c72348b
Kamil Delekta <kamil.delekta@C-Q7VDWQC44L.chime.local>
2022-03-03 17:20:11 +0100

    Adding ScrollView And Keyboard Avoiding View

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,4 +1,12 @@
-import { SafeAreaView, Text, StyleSheet, View, Keyboard } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  Keyboard,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
 import React, { Component } from "react";
 import { TextInput } from "react-native-gesture-handler";
 
@@ -23,54 +31,70 @@ export class SettingsScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Settings</Text>
-        </View>
-        <View>
-          <Text style={styles.settingResult}>Setting 1: {this.state.set1}</Text>
-          <Text style={styles.settingResult}>Setting 2: {this.state.set2}</Text>
-          <Text style={styles.settingResult}>Setting 3: {this.state.set3}</Text>
-          <Text style={styles.settingResult}>Setting 4: {this.state.set4}</Text>
-        </View>
-        <View>
-          <View style={styles.inputRow}>
-            <Text style={styles.settingResult}>Input 1: </Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={(text: string) => this.setState({ set1: text })}
-              autoFocus={true}
-              onSubmitEditing={() => this.ref2.current?.focus()}
-            />
+      <KeyboardAvoidingView
+        style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}
+        behavior="padding"
+        enabled
+      >
+        <ScrollView>
+          <View style={styles.container}>
+            <View style={styles.header}>
+              <Text style={styles.headerText}>Settings</Text>
+            </View>
+            <View>
+              <Text style={styles.settingResult}>
+                Setting 1: {this.state.set1}
+              </Text>
+              <Text style={styles.settingResult}>
+                Setting 2: {this.state.set2}
+              </Text>
+              <Text style={styles.settingResult}>
+                Setting 3: {this.state.set3}
+              </Text>
+              <Text style={styles.settingResult}>
+                Setting 4: {this.state.set4}
+              </Text>
+            </View>
+            <View>
+              <View style={styles.inputRow}>
+                <Text style={styles.settingResult}>Input 1: </Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={(text: string) => this.setState({ set1: text })}
+                  autoFocus={true}
+                  onSubmitEditing={() => this.ref2.current?.focus()}
+                />
+              </View>
+              <View style={styles.inputRow}>
+                <Text style={styles.settingResult}>Input 2: </Text>
+                <TextInput
+                  ref={this.ref2}
+                  style={styles.input}
+                  onChangeText={(text: string) => this.setState({ set2: text })}
+                  onSubmitEditing={() => this.ref3.current?.focus()}
+                />
+              </View>
+              <View style={styles.inputRow}>
+                <Text style={styles.settingResult}>Input 3: </Text>
+                <TextInput
+                  ref={this.ref3}
+                  style={styles.input}
+                  onChangeText={(text: string) => this.setState({ set3: text })}
+                  onSubmitEditing={() => this.ref4.current?.focus()}
+                />
+              </View>
+              <View style={styles.inputRow}>
+                <Text style={styles.settingResult}>Input 4: </Text>
+                <TextInput
+                  ref={this.ref4}
+                  style={styles.input}
+                  onChangeText={(text: string) => this.setState({ set4: text })}
+                />
+              </View>
+            </View>
           </View>
-          <View style={styles.inputRow}>
-            <Text style={styles.settingResult}>Input 2: </Text>
-            <TextInput
-              ref={this.ref2}
-              style={styles.input}
-              onChangeText={(text: string) => this.setState({ set2: text })}
-              onSubmitEditing={() => this.ref3.current?.focus()}
-            />
-          </View>
-          <View style={styles.inputRow}>
-            <Text style={styles.settingResult}>Input 3: </Text>
-            <TextInput
-              ref={this.ref3}
-              style={styles.input}
-              onChangeText={(text: string) => this.setState({ set3: text })}
-              onSubmitEditing={() => this.ref4.current?.focus()}
-            />
-          </View>
-          <View style={styles.inputRow}>
-            <Text style={styles.settingResult}>Input 4: </Text>
-            <TextInput
-              ref={this.ref4}
-              style={styles.input}
-              onChangeText={(text: string) => this.setState({ set4: text })}
-            />
-          </View>
-        </View>
-      </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     );
   }
 }
